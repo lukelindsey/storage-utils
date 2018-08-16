ab14bd2896a97b0fbc9ce1314f971a1a5af77220
Luke Lindsey <luke.lindsey.dev@gmail.com>
2018-08-15 21:54:09 -0500

    feat: Add millisecond conversion functions

---
+++ src/millisecond-conversions.ts
@@ -0,0 +1,15 @@
+export function convertDaysToMilliseconds(days: number) {
+  return days * 24 * 60 * 60 * 1000
+}
+
+export function convertHoursToMilliseconds(hours: number) {
+  return hours * 60 * 60 * 1000
+}
+
+export function convertMinutesToMilliseconds(minutes: number) {
+  return minutes * 60 * 1000
+}
+
+export function convertSecondsToMilliseconds(seconds: number) {
+  return seconds * 1000
+}
