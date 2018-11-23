ecc5bb23b0026237a107661e39996cba3487b654
Luke Lindsey <luke.lindsey.dev@gmail.com>
2018-11-22 20:42:25 -0600

    test: Add useResolve tests
    tests for #1

--- test/useResolve.test.ts
+++ test/useResolve.test.ts
@@ -1,5 +1,5 @@
 import "core-js/es6/promise";
-import { getOrAdd } from "../src/storage-utils";
+import { useResolve } from "../src/storage-utils";
 import { cacheKey, fromCacheExpected, getLastSet, mockCached } from "./testUtils";
 
 const fromFetchExpected = [2, 4, 6];
@@ -15,19 +15,26 @@ const doAnotherFetch = () => {
   return Promise.resolve(notExpected);
 };
 
-describe("getOrAdd function", () => {
+let resolved: number[][] = [];
+const resolve = (data: number[]) => {
+  resolved.push(data);
+};
+
+describe("useResolve function", () => {
   beforeEach(() => {
     fetchCalls = 0;
+    resolved = [];
     localStorage.clear();
     (localStorage.setItem as any).mockClear();
     (localStorage.getItem as any).mockClear();
   });
 
-  it("should retrieve from the cache if it's present and not expired", done => {
+  it("should retrieve only from the cache if it's present and not expired", done => {
     mockCached(100);
     const ttl = 5000;
-    getOrAdd(localStorage, cacheKey, doFetch, ttl).then(result => {
-      expect(result).toEqual(fromCacheExpected);
+    useResolve(localStorage, cacheKey, doFetch, ttl, resolve).then(() => {
+      expect(resolved[0]).toEqual(fromCacheExpected);
+      expect(resolved.length).toEqual(1);
       expect(localStorage.getItem).toHaveBeenCalledTimes(1);
       expect(localStorage.getItem).toHaveBeenCalledWith(cacheKey);
       expect(localStorage.setItem).not.toHaveBeenCalled();
@@ -36,12 +43,13 @@ describe("getOrAdd function", () => {
     });
   });
 
-  it("should update cache with fetched even if using expired", done => {
+  it("should resolve from cache then fetch and update cache", done => {
     mockCached(-1);
     const ttl = 5000;
-    const useExpired = true;
-    getOrAdd(localStorage, cacheKey, doFetch, ttl, useExpired).then(result => {
-      expect(result).toEqual(fromCacheExpected);
+    useResolve(localStorage, cacheKey, doFetch, ttl, resolve).then(() => {
+      expect(resolved[0]).toEqual(fromCacheExpected);
+      expect(resolved[1]).toEqual(fromFetchExpected);
+      expect(resolved.length).toEqual(2);
       expect(localStorage.getItem).toHaveBeenCalledTimes(1);
       expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
       const set = getLastSet();
@@ -54,11 +62,11 @@ describe("getOrAdd function", () => {
     });
   });
 
-  it("should fetch if it's in cache and expired", done => {
-    mockCached(-1);
+  it("should resolve only from fetch if it's not in cache", done => {
     const ttl = 5000;
-    getOrAdd(localStorage, cacheKey, doFetch, ttl).then(result => {
-      expect(result).toEqual(fromFetchExpected);
+    useResolve(localStorage, cacheKey, doFetch, ttl, resolve).then(() => {
+      expect(resolved[0]).toEqual(fromFetchExpected);
+      expect(resolved.length).toEqual(1);
       expect(localStorage.getItem).toHaveBeenCalledTimes(1);
       expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
@@ -72,27 +80,11 @@ describe("getOrAdd function", () => {
     });
   });
 
-  it("should fetch if it's not in cache", done => {
-    const ttl = 5000;
-    getOrAdd(localStorage, cacheKey, doFetch, ttl).then(result => {
-      expect(result).toEqual(fromFetchExpected);
-      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
-      expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
-      expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-      const set = getLastSet();
-      expect(set.key).toEqual(cacheKey);
-      expect(set.data).toEqual(fromFetchExpected);
-      // expiration set to within a second of what we expect
-      expect((new Date().getTime() + ttl) / 2000 - set.expiration / 2000).toBeCloseTo(0, 0);
-      expect(fetchCalls).toEqual(1);
-      done();
-    });
-  });
-  it("should fetch if it's not in cache and useExpired is true", done => {
-    const ttl = 5000;
-    const useExpired = true;
-    getOrAdd(localStorage, cacheKey, doFetch, ttl, useExpired).then(result => {
-      expect(result).toEqual(fromFetchExpected);
+  it("should sucessfully cache", done => {
+    const ttl = 10000;
+    useResolve(localStorage, cacheKey, doFetch, ttl, resolve).then(() => {
+      expect(resolved[0]).toEqual(fromFetchExpected);
+      expect(resolved.length).toEqual(1);
       expect(localStorage.getItem).toHaveBeenCalledTimes(1);
       expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
       expect(localStorage.setItem).toHaveBeenCalledTimes(1);
@@ -100,28 +92,15 @@ describe("getOrAdd function", () => {
       expect(set.key).toEqual(cacheKey);
       expect(set.data).toEqual(fromFetchExpected);
       // expiration set to within a tenth of a second of what we expect
-      expect((new Date().getTime() + ttl) / 2000 - set.expiration / 2000).toBeCloseTo(0, 0);
       expect(fetchCalls).toEqual(1);
-      done();
-    });
-  });
 
-  it("should cache indefinitely if no ttl is provided", done => {
-    getOrAdd(localStorage, cacheKey, doFetch).then(result => {
-      expect(result).toEqual(fromFetchExpected);
-      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
-      expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
-      expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-      const set = getLastSet();
-      expect(set.key).toEqual(cacheKey);
-      expect(set.data).toEqual(fromFetchExpected);
-      // expiration set to within a tenth of a second of what we expect
-      expect(set.expiration).toBeFalsy();
-      expect(fetchCalls).toEqual(1);
+      // reset the resolved to make testing below simpler
+      resolved = [];
 
       // get again and make sure we don't fetch
-      getOrAdd(localStorage, cacheKey, doAnotherFetch).then(result => {
-        expect(result).toEqual(fromFetchExpected); // from the first fetch :)
+      useResolve(localStorage, cacheKey, doAnotherFetch, ttl, resolve).then(() => {
+        expect(resolved[0]).toEqual(fromFetchExpected); // from the first fetch :)
+        expect(resolved.length).toEqual(1);
         expect(localStorage.getItem).toHaveBeenCalledTimes(2);
         expect(localStorage.getItem).toHaveBeenLastCalledWith(cacheKey);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
